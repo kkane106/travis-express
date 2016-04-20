8d945587347fdafd48ed39f194e7d047c64e631e
Kris Kane <kkane106@gmail.com>
2016-04-20 17:13:27 -0600

    Configure mongoose

---
+++ app_api/models/db.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+var dbLocation = 'mongodb://localhost/heroku';
+if (process.env.NODE_ENV === 'production') {
+	dbLocation = process.env.MONGODB_URI;
+}
+mongoose.connect(dbLocation);
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbLocation);
+});
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+// For nodemon restarts
+process.once('SIGUSR2', function () {
+  gracefulShutdown('nodemon restart', function () {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+// For app termination
+process.on('SIGINT', function() {
+  gracefulShutdown('app termination', function () {
+    process.exit(0);
+  });
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+  gracefulShutdown('Heroku app termination', function () {
+    process.exit(0);
+  });
+});
+
+require('./todos');
